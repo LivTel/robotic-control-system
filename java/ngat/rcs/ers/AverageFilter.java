68c87377fe11d0ba23a4b980df35c4b459b7d54a
Chris Mottram <C.Mottram@ljmu.ac.uk>
2015-05-21 12:47:50 +0100

    Added logging to java/ngat/rcs/ers/AverageFilter.java.

--- java/ngat/rcs/ers/AverageFilter.java
+++ java/ngat/rcs/ers/AverageFilter.java
@@ -6,6 +6,10 @@ package ngat.rcs.ers;
 import java.util.List;
 import java.util.Vector;
 
+import ngat.util.logging.LogGenerator;
+import ngat.util.logging.LogManager;
+import ngat.util.logging.Logger;
+
 /**
  * @author eng
  * 
@@ -15,15 +19,25 @@ public class AverageFilter extends ContinuousFilter {
 	protected List<Sample> samples;
 
 	protected long timespan;
+	
+	/** Logger. */
+	private transient LogGenerator slogger;
 
 	/**
 	 * @param name The name of the filter.
 	 * @param timespan The period to extract the average from.
 	 */
-	public AverageFilter(String name, long timespan) {
+	public AverageFilter(String name, long timespan) 
+	{
 		super(name);
 		this.timespan = timespan;
 		samples = new Vector<Sample>();
+		
+		Logger alogger = LogManager.getLogger("ERS"); 
+		slogger = alogger.generate().system("RCS")
+				.subSystem("Reactive")
+				.srcCompClass(this.getClass().getSimpleName())
+				.srcCompId(name);
 	}
 
 	@Override
@@ -38,6 +52,16 @@ public class AverageFilter extends ContinuousFilter {
 			fvalue += samples.get(i).value.doubleValue();
 		}
 		fvalue /= Math.max(1.0, samples.size());
+		
+		slogger.create().info().level(3).block("processUpdate")
+		.msg(String.format("%6s time=%tT timeSpan=%tT samplesize=%6d value=%6.2f average value=%6.2f \n",
+			name, 
+			time,
+			timespan,
+			samples.size(),
+			dvalue,
+			fvalue)).send();
+		
 		return fvalue;
 
 	}
